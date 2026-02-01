78ecf119d5571c1a81c96d1bede11dcc9ade535f
Mert <37841888+safaktepe@users.noreply.github.com>
2026-02-01 15:34:51 +0100

    Fix scroll position on project detail navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ScrollToTop } from "./components/ScrollToTop";
 import { LandingPage } from "./pages/LandingPage";
 import { ProjectDetailPage } from "./pages/ProjectDetailPage";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/projects/:slug" element={<ProjectDetailPage />} />
